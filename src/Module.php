60629696a09d517c1e4bfd909a7c23ae83b5a8a4
Andréas Livet <andreas.livet@gmail.com>
2015-06-09 14:18:54 +0200

    Modules are now autoconfigured with the config/module.php file

--- src/Module.php
+++ src/Module.php
@@ -2,22 +2,35 @@
 /**
  * Class that simplify module creation and which module can derivate from
  */
+use Config;
+use Lang;
+
 class Module extends \Module {
 	public $file;
 	public function __construct() {
-		if(isset($this->file)){
-			$dir = strtolower(dirname($this->file));
+		$trace = debug_backtrace()[0];
+		//Retrieve the file informations
+		//To check if the module filename and directory are the same
+		if(isset($trace['file'])){
+			$dir = strtolower(dirname($trace['file']));
 			$dir = last(explode('/', $dir));
-			$base = strtolower(basename($this->file, '.php'));
+			$base = strtolower(basename($trace['file'], '.php'));
 			if($dir == $base){
 				$this->name = $base;
 			}
 			else {
-				d('Error dirname and filename are not the same : '.$dir.' and '.$base);
+				dd('Error dirname and filename are not the same : '.$dir.' and '.$base);
 			}
 		}
 		//Illuminato module need to be install before installing this one
 		$this->dependencies = array('illuminato');
+		$this->tab = Config::get('module.tab');
+		$this->version = Config::get('module.version');
+		$this->author = Config::get('module.author');
+		//Module are bootstrap 'aware' by default
+		$this->bootstrap = true;
 		parent::__construct();
+		$this->displayName = Lang::get(Config::get('module.displayName'));
+		$this->description = Lang::get(Config::get('module.description'));
 	}
 }
